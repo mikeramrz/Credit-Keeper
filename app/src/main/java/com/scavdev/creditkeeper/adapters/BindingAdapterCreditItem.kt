975457dac8a18953a9f5b62a1cc7b00b80720519
michaelramirez <mramirezt4@hotmail.com>
2020-05-27 22:39:52 -0500

    Binding adapters for double values

--- app/src/main/java/com/scavdev/creditkeeper/adapters/BindingAdapterCreditItem.kt
+++ app/src/main/java/com/scavdev/creditkeeper/adapters/BindingAdapterCreditItem.kt
@@ -3,6 +3,8 @@ package com.scavdev.creditkeeper.adapters
 import android.icu.text.SimpleDateFormat
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import com.google.android.material.textfield.TextInputEditText
 import com.scavdev.creditkeeper.R
 import java.util.*
 
@@ -23,20 +25,37 @@ fun setDateFormat(textView: TextView, dueDate: Date) {
     val todayDate = Calendar.getInstance(TimeZone.getDefault())
     val dueDateDay = SimpleDateFormat("dd").format(dueDate.time)
 
-    if (hasDayPassedThisMonth(dueDate, todayDate)){
+    if (hasDayPassedThisMonth(dueDate, todayDate)) {
         todayDate.add(Calendar.MONTH, 1)
         val nextMonthString = SimpleDateFormat("MMM").format(todayDate.time)
         val textDateString = " Due: $nextMonthString $dueDateDay"
         textView.text = textDateString
-    }
-    else{
+    } else {
         val monthString = SimpleDateFormat("MMM").format(todayDate.time)
         val textDateString = " Due: $monthString $dueDateDay"
         textView.text = textDateString
     }
 }
 
-private fun hasDayPassedThisMonth(dueDate: Date, todayDate:Calendar) :Boolean{
+
+@BindingAdapter("android:text")
+fun setDouble(inputEditText: TextInputEditText, double: Double) {
+    if (double.equals(Double.NaN)) {
+        inputEditText.setText("")
+    } else inputEditText.setText(double.toString())
+}
+
+@InverseBindingAdapter(attribute = "android:text")
+fun getDouble(textInputEditText: TextInputEditText): Double {
+    var num: String = textInputEditText.text.toString();
+    return if (num.isEmpty()) {
+        "0".toDouble()
+    } else {
+        num.toDouble()
+    }
+}
+
+private fun hasDayPassedThisMonth(dueDate: Date, todayDate: Calendar): Boolean {
     val cal = Calendar.getInstance()
     cal.time = dueDate
 
